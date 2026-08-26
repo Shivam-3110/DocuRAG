2c653203e0d295be5455870246b05069ccf9ec51
Shivam-3110 <mrshivam00022@gmail.com>
2026-08-26 21:42:32 +0530

    Server setup and pdf upload Api

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const multer = require('multer');
+const fs = require('fs');
+const app = express();
+const PORT = process.env.PORT || 3000;
+const pdfParse = require('pdf-parse');
+
+const upload = multer({ dest: 'Uploads/' });
+app.get("/",(req,res)=>{
+    res.send("server is running");
+})
+
+app.post('/upload',upload.single('pdf'),async (req,res)=>{
+        console.log(req.file);
+    const dataBuffer = fs.readFileSync(req.file.path);
+    const pdfData = await pdfParse(dataBuffer);
+    const text = pdfData.text;
+        res.send({text});
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
