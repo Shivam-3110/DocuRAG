e8582d9a9a05dcd6f9e038746292c2dad5abd519
Shivam-3110 <mrshivam00022@gmail.com>
2026-09-02 22:23:37 +0530

    "embedding model working"

--- server/index.js
+++ server/index.js
@@ -9,19 +9,37 @@ const upload = multer({ dest: 'Uploads/' });
 require('dotenv').config();
 const ai = new GoogleGenAI(process.env.GEMINI_API_KEY);
 
+async function createEmbedding(text) {
+    const response = await ai.models.embedContent({
+        model: "gemini-embedding-2",
+        contents: text,
+        config: {
+            outputDimensionality: 768
+        }
+    });
+
+    return response.embeddings[0].values;
+}
+
 app.get("/",(req,res)=>{
     res.send("server is running");
 })
 
 app.post('/upload',upload.single('pdf'),async (req,res)=>{
         console.log(req.file);
+        console.log(req.body);
         try {
     const dataBuffer = fs.readFileSync(req.file.path);
     const pdfData = await pdfParse(dataBuffer);
     const text = pdfData.text;
-    const chunks = text.split('\n\n');
+    const chunks = text.split('\n\n').filter(chunk => chunk.trim() !== '');
+      console.log(chunks);
+    const embedding = await createEmbedding(chunks[0]);
+    console.log('Embedding:', embedding);
+    const question = req.body.question;
+    console.log(question);
     const response = await ai.models.generateContent({
-        model: 'gemini-2.5-flash-lite',
+        model: 'gemini-3.5-flash-lite',
         contents: `Explain this pdf in simple text: ${chunks[1]}`,
         })
       res.send(response.text);
